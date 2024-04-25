5a876a620a9d764f9ae53f353ef94acdb79d0e16
nikitafloy <nikita5051@yandex.ru>
2024-04-25 21:16:14 +0700

    add tests for file

---
+++ tests/file.test.ts
@@ -0,0 +1,69 @@
+import request from "supertest";
+import { app, server } from "../express";
+import { createTestUser } from "./helpers";
+import FormData from "form-data";
+import fs from "node:fs";
+import path from "node:path";
+
+describe("file controller", () => {
+  const email = "nikita@mail.ru";
+  const password = "password";
+
+  let accessToken: string;
+
+  beforeAll(async () => {
+    await prisma.file.deleteMany();
+    await prisma.userSessions.deleteMany();
+    await prisma.user.deleteMany();
+
+    const testUserTokens = await createTestUser(email, password);
+
+    accessToken = testUserTokens.accessToken;
+  });
+
+  test("list should return right files length for new user", async () => {
+    const list = await request(app)
+      .get("/file/list")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .expect(200);
+
+    expect(list.body.message.files.length).toBe(0);
+  });
+
+  test("upload should return success uploaded file", async () => {
+    const fileName = "lorem ipsum";
+
+    const fileData = fs.readFileSync(path.join(__dirname, "files", fileName));
+
+    const formData = new FormData();
+    formData.append("file", fileData, fileName);
+
+    await request(app)
+      .post("/file/upload")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .set(
+        "Content-Type",
+        `multipart/form-data; boundary=${formData.getBoundary()}`,
+      )
+      .send(formData.getBuffer())
+      .expect(204);
+
+    const user = await prisma.user.findFirst({
+      where: { id: email },
+    });
+
+    if (!user) {
+      fail("User was not found");
+    }
+
+    const count = await prisma.file.count({
+      where: { userId: user.userId },
+    });
+
+    expect(count).toBe(1);
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+});
