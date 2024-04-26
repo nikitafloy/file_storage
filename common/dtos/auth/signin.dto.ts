62d50d6cd343d48ffd0187fff6d5e71938d999c8
nikitafloy <nikita5051@yandex.ru>
2024-04-26 21:45:55 +0700

    fix order for imports

--- common/dtos/auth/signin.dto.ts
+++ common/dtos/auth/signin.dto.ts
@@ -1,5 +1,5 @@
-import { SignupDto } from "./signup.dto";
 import { IsNotEmpty, IsString } from "class-validator";
+import { SignupDto } from "./signup.dto";
 
 export class SigninDto extends SignupDto {
   @IsNotEmpty()
